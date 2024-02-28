259e612ba6db8abf500ac9f50b1a70089e354d63
Brijesh Kumar Singh <132880648+b02k11@users.noreply.github.com>
2024-02-28 21:15:24 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL,{
+    dbName:"apply",
+});
 
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 const db=mongoose.connection;
