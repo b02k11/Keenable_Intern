81cb6ab9b4f30fb326de6c77689f1e430aa0b496
Brijesh Kumar Singh <132880648+b02k11@users.noreply.github.com>
2024-02-28 21:45:33 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/web_database');
+// mongoose.connect('mongodb://127.0.0.1:27017/web_database');
+mongoose.connect('mongodb+srv://brijeshk:brijeshk@cluster0.ztovedn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+    dbName:"apply",
+});
 
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 const db=mongoose.connection;
